ea1f21b73935eddcb1e309797425758e4fd21ccd
fpolanco <polanco.franchell@gmail.com>
2021-04-28 21:36:21 -0400

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 
 import './App.css';
-//import indexhtml from 'public./index.html';
+import './App.scss';
+
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
+  Button
+} from "shards-react";
+
+// styles
+import { Alert } from "shards-react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+// import Modal from "./Modal";
+
+// import ModalBody from "./ModalBody";
+// import ModalHeader from "./ModalHeader";
+// import ModalFooter from "./ModalFooter";
+
+// export { Modal };
+//ModalBody, ModalHeader, ModalFooter };
+ //import Navbar from "./Navbar";
+// import NavbarBrand from "./NavbarBrand";
+// import NavbarToggler from "./NavbarToggler";
+
+// export { Navbar, NavbarBrand, NavbarToggler };
+
+
 
 function App() {
   return (
     <div className="App">
-      
-      <h1> franchell.tech
-
-      </h1>
-      <p> My website </p>
+export default function BasicCardExample() {
+  //return (
+    <Card style={{ maxWidth: "300px" }}>
+      <CardHeader>Card header</CardHeader>
+      <CardImg src="https://place-hold.it/300x200" />
+      <CardBody>
+        <CardTitle>Lorem Ipsum</CardTitle>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <Button>Read more &rarr;</Button>
+      </CardBody>
+      <CardFooter>Card footer</CardFooter>
+    </Card>
+ // );
+}
     </div>
   );
 }
